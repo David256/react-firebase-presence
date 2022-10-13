074db57fc3bc799aab0754ead91d3991f31bd65e
David256 <wasterdavid@gmail.com>
2022-10-13 09:16:34 -0500

    Add base directory structure

---
+++ src/components/LOL/LOL.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export interface LOLProps {};
+
+const LOL: React.FunctionComponent<LOLProps> = (props) => {
+  return <span>Lol</span>
+}
+
+export default LOL;
---
+++ src/components/index.ts
@@ -0,0 +1,1 @@
+export { default as LOL } from './LOL';
